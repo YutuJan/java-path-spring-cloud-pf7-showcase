16ec203eae106b92fc70bfe923d8667da316516e
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-01-10 20:12:08 +0200

    Disable Database and JPA autoconfiguration.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-cloud-mail-service/src/main/java/gr/codelearn/spring/cloud/showcase/mail/MailApplication.java
+++ java-path-spring-cloud-mail-service/src/main/java/gr/codelearn/spring/cloud/showcase/mail/MailApplication.java
@@ -2,9 +2,12 @@ package gr.codelearn.spring.cloud.showcase.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @ComponentScan(basePackages = {"gr.codelearn.spring.cloud.showcase.core", "gr.codelearn.spring.cloud.showcase.mail"})
 public class MailApplication {
 	public static void main(String[] args) {
