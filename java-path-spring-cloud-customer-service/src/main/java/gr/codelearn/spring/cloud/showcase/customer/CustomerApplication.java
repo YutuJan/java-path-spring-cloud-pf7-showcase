27cc8cf307c82f6d34b45d270e0e86b64edb1c4d
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-01-10 19:06:01 +0200

    Moved query to order microservice as it seems more suitable location according to be business criteria used to generate the order microservice.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-cloud-customer-service/src/main/java/gr/codelearn/spring/cloud/showcase/customer/CustomerApplication.java
+++ java-path-spring-cloud-customer-service/src/main/java/gr/codelearn/spring/cloud/showcase/customer/CustomerApplication.java
@@ -3,11 +3,9 @@ package gr.codelearn.spring.cloud.showcase.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"gr.codelearn.spring.cloud.showcase.core", "gr.codelearn.spring.cloud.showcase.customer"})
-@EnableJpaRepositories(namedQueriesLocation = "classpath:jpa-named-queries.properties")
 public class CustomerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
--- java-path-spring-cloud-customer-service/src/main/java/gr/codelearn/spring/cloud/showcase/customer/service/CustomerService.java
+++ java-path-spring-cloud-customer-service/src/main/java/gr/codelearn/spring/cloud/showcase/customer/service/CustomerService.java
@@ -1,16 +1,8 @@
 package gr.codelearn.spring.cloud.showcase.customer.service;
 
 import gr.codelearn.spring.cloud.showcase.core.service.BaseService;
-import gr.codelearn.spring.cloud.showcase.core.transfer.KeyValue;
 import gr.codelearn.spring.cloud.showcase.customer.domain.Customer;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 public interface CustomerService extends BaseService<Customer, Long> {
 	Customer findByEmail(String email);
-
-	List<KeyValue<String, BigDecimal>> findCustomersPurchasedMostExpensiveProduct();
-
-	List<PurchasesPerCustomerCategoryDto> findTotalNumberAndCostOfPurchasesPerCustomerCategory();
 }
