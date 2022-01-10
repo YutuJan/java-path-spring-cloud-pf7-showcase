18fe72bc835f7c8d980ffa8f08135b6a7a68a233
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-01-10 13:04:14 +0200

    Added @ComponentScan in order to instantiate Spring beans inherited from core library.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/CatalogApplication.java
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/CatalogApplication.java
@@ -2,9 +2,11 @@ package gr.codelearn.spring.cloud.showcase.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"gr.codelearn.spring.cloud.showcase.core", "gr.codelearn.spring.cloud.showcase.catalog"})
 @EnableJpaRepositories(namedQueriesLocation = "classpath:jpa-named-queries.properties")
 public class CatalogApplication {
 	public static void main(String[] args) {
