fe3530617b8b262590696c5165c3c1ceff4b3196
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-01-11 12:24:01 +0200

    Introduced corresponding mappers and controllers. Also, some controllers were renamed following the naming convention "*ResourceController" to depict the type of information returned.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/controller/ProductResourceController.java
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/controller/ProductResourceController.java
@@ -1,10 +1,13 @@
 package gr.codelearn.spring.cloud.showcase.catalog.controller;
 
 import gr.codelearn.spring.cloud.showcase.catalog.domain.Product;
+import gr.codelearn.spring.cloud.showcase.catalog.mapper.ProductMapper;
 import gr.codelearn.spring.cloud.showcase.catalog.service.ProductService;
+import gr.codelearn.spring.cloud.showcase.core.base.BaseMapper;
 import gr.codelearn.spring.cloud.showcase.core.controller.AbstractController;
 import gr.codelearn.spring.cloud.showcase.core.service.BaseService;
 import gr.codelearn.spring.cloud.showcase.core.transfer.ApiResponse;
+import gr.codelearn.spring.cloud.showcase.core.transfer.resource.ProductResource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
-public class ProductController extends AbstractController<Product> {
+public class ProductResourceController extends AbstractController<Product, ProductResource> {
 	private final ProductService productService;
+	private final ProductMapper productMapper;
 
 	@Override
 	public BaseService<Product, Long> getBaseService() {
 		return productService;
+	}
+
+	@Override
+	protected BaseMapper<Product, ProductResource> getMapper() {
+		return productMapper;
 	}
 
 	@GetMapping(params = {"serial"})
--- java-path-spring-cloud-customer-service/src/main/java/gr/codelearn/spring/cloud/showcase/customer/controller/CustomerController.java
+++ java-path-spring-cloud-customer-service/src/main/java/gr/codelearn/spring/cloud/showcase/customer/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package gr.codelearn.spring.cloud.showcase.customer.controller;
 
+import gr.codelearn.spring.cloud.showcase.core.base.BaseMapper;
 import gr.codelearn.spring.cloud.showcase.core.controller.AbstractController;
 import gr.codelearn.spring.cloud.showcase.core.service.BaseService;
 import gr.codelearn.spring.cloud.showcase.core.transfer.ApiResponse;
+import gr.codelearn.spring.cloud.showcase.core.transfer.resource.CustomerResource;
 import gr.codelearn.spring.cloud.showcase.customer.domain.Customer;
+import gr.codelearn.spring.cloud.showcase.customer.mapper.CustomerMapper;
 import gr.codelearn.spring.cloud.showcase.customer.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +18,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/customers")
-public class CustomerController extends AbstractController<Customer> {
+public class CustomerController extends AbstractController<Customer, CustomerResource> {
 	private final CustomerService customerService;
+	private final CustomerMapper customerMapper;
 
 	@Override
 	public BaseService<Customer, Long> getBaseService() {
 		return customerService;
 	}
 
-	@GetMapping(params = {"email"})
-	public ResponseEntity<ApiResponse<Customer>> findByEmail(@RequestParam String email) {
-		return ResponseEntity.ok(ApiResponse.<Customer>builder().data(customerService.findByEmail(email)).build());
+	@Override
+	public BaseMapper<Customer, CustomerResource> getMapper() {
+		return customerMapper;
 	}
+
+	@GetMapping(params = {"email"})
+	public ResponseEntity<ApiResponse<CustomerResource>> findByEmail(@RequestParam String email) {
+		return ResponseEntity.ok(ApiResponse.<CustomerResource>builder()
+										 .data(customerMapper.toResource(customerService.findByEmail(email))).build());
+	}
+
+	//	@GetMapping(params = {"email"})
+	//	public ResponseEntity<ApiResponse<Customer>> findByEmail(@RequestParam String email) {
+	//		return ResponseEntity.ok(ApiResponse.<Customer>builder().data(customerService.findByEmail(email)).build());
+	//	}
 }
